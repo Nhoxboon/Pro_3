f44f3ea3ec18d810d86cc0f768d85b5cee018c9a
Nhoxboon <trannhatanh156@gmail.com>
2025-03-21 19:36:19 +0700

    fix a little logic

--- Assets/_Data/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
+++ Assets/_Data/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
@@ -66,7 +66,7 @@ public class PlayerGroundedState : PlayerState
         {
             stateMachine.ChangeState(player.PlayerJumpState);
         }
-        else if (!isGrounded && isTouchingCeiling && !isTouchingCeiling)
+        else if (!isGrounded)
         {
             player.PlayerInAirState.StartCoyoteTime();
             stateMachine.ChangeState(player.PlayerInAirState);
